286a8a82c5144eed06bffc22d9230dcf527083ed
Muhammad Abubakar <85725957+itsBakar@users.noreply.github.com>
2022-09-07 00:05:56 -0700

    Six commit

--- pages/Name.js
+++ pages/Name.js
@@ -1,12 +1,11 @@
-import { Paper, Typography, Box, Divider } from '@mui/material'
+import { Paper, Typography, Box, Divider, Avatar } from '@mui/material'
 import Image from 'next/image'
 import React from 'react'
-import Pic from '../public/Pic2.jpg'
 const Name = () => {
   return (
     <div>
             <Box display={'flex'} alignItems={'center'} gap={5}>
-            <Image style={{borderRadius:'50%'}} height={150} width={150} src={Pic} alt='picture' />
+            <Avatar sx={{height:'150px', width:'150px'}}>MA</Avatar>
             <Box>
             <Typography variant='h3'>Its <Typography component={'span'} variant='h3' color={'primary'}>Bakar</Typography></Typography>
             <Typography variant='subtitle'>Software Engineer</Typography>
